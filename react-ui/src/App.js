d570919ce275d48fa2114a3e0a77fd515d87420f
johnny <superstar41668087@gmail.com>
2021-03-25 21:42:08 -0400

    data type added

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,11 +1,22 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Spinner from "react-bootstrap/Spinner";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
 import "./App.css";
 //
 function App() {
   const [data, setData] = useState({});
   const [showLoading, setShowLoading] = useState(true);
+  const [flowerData, setFlowerData] = useState({
+    sepal_length: "",
+    sepal_width: "",
+    petal_length: "",
+    petal_width: "",
+    species: "",
+  });
   const apiUrl = "http://localhost:3000/run";
   //runs once after the first rendering of page
   useEffect(() => {
@@ -23,6 +34,15 @@ function App() {
     };
     fetchData();
   }, []);
+
+  const onChange = (e) => {
+    e.persist();
+    setFlowerData({ ...flowerData, [e.target.name]: e.target.value });
+  };
+
+  const getSpecies = () => {
+    axios.post();
+  };
 
   return (
     <div>
@@ -69,6 +89,97 @@ function App() {
               </tr>
             </tbody>
           </table>
+          <br />
+          <div>
+            {" "}
+            <Jumbotron>
+              <Form onSubmit={getSpecies}>
+                <Form.Group>
+                  <Form.Label>Student Number</Form.Label>
+                  <Form.Control
+                    type="number"
+                    name="studentNumber"
+                    id="studentNumber"
+                    placeholder="Enter student number"
+                    value={flowerData.sepal_length}
+                    onChange={onChange}
+                  />
+                </Form.Group>
+
+                <Form.Group>
+                  <Form.Label> First Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="firstName"
+                    id="firstName"
+                    placeholder="Enter first name"
+                    value={flowerData.sepal_width}
+                    onChange={onChange}
+                  />
+                </Form.Group>
+
+                <Form.Group>
+                  <Form.Label> Last Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="lastName"
+                    id="lastName"
+                    placeholder="Enter last name"
+                    value={flowerData.petal_length}
+                    onChange={onChange}
+                  />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Address</Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="address"
+                    id="address"
+                    placeholder="Enter address"
+                    value={flowerData.petal_width}
+                    onChange={onChange}
+                  />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>City</Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="city"
+                    id="city"
+                    placeholder="Enter city"
+                    value={flowerData.species}
+                    onChange={onChange}
+                  />
+                </Form.Group>
+                {/* <Form.Group>
+                  <Form.Label>phone</Form.Label>
+                  <Form.Control
+                    type="number"
+                    name="phone"
+                    id="phone"
+                    placeholder="Enter first name"
+                    value={user.phone}
+                    onChange={onChange}
+                  />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="email"
+                    id="email"
+                    rows="3"
+                    placeholder="Enter email"
+                    value={user.email}
+                    onChange={onChange}
+                  />
+                </Form.Group> */}
+                <Button variant="primary" type="submit">
+                  Save
+                </Button>
+              </Form>
+            </Jumbotron>
+          </div>
         </div>
       ) : (
         <div>
